44ab0f0a2479f051eafdc59f2fd536f1b56f87e0
matheusgeber <matheusgeber@gmail.com>
2026-06-11 00:24:46 -0300

    first commit

---
+++ src/main/java/com/scvp/ScvpApplication.java
@@ -0,0 +1,13 @@
+package com.scvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scvp/ScvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
